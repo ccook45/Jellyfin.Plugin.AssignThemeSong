56baf959e98fc41906c64a1c86859908f221c081
ccook45 <83917888+ccook45@users.noreply.github.com>
2026-09-20 00:48:21 -0400

    Restore Task using for Jellyfin 12 auth fix

--- Api/ThemeSongController.cs
+++ Api/ThemeSongController.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Claims;
 using System.Text.Json;
 using Jellyfin.Data;
 using Jellyfin.Data.Enums;
